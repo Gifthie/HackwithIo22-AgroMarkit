204c9c153564264da0bd27bbcf87748b56081718
Kayode <khaox07@gmail.com>
2022-07-12 15:48:17 +0100

    Pushed to Github

---
+++ src/layouts/app.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <Navbar />
+        <main class="max-w-5xl p-5 mx-auto my-10 text-center">
+            <router-view />
+        </main>
+    </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- <h1 class="text-2xl font-bold">Welcome to FarmMarkit</h1> -->
+    <div></div>
+</template>
+
+<route lang="yaml">
+meta:
+    layout: app
+</route>
+
+<script setup>
+import { useMeta } from 'vue-meta'
+
+useMeta({
+    title: 'Homepage',
+})
+</script>
