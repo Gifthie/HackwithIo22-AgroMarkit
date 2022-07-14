ea1ccf9c499c3764a5d59243a8910077ba461585
Kayode <khaox07@gmail.com>
2022-07-14 17:08:29 +0100

    Added cart button to mobile

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -115,6 +115,12 @@
                                     class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                                     >{{ item.name }}</a
                                 >
+                                <button @click="openCart" class="flex items-center bg-green-500 rounded-full px-3 py-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                    </svg>
+                                    <span class="bg-white rounded-full px-3 font-bold">{{ cart.length }}</span>
+                                </button>
                                 
                             </div>
                         </div>
